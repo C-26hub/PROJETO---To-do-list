b061d5a1cf86ea8d89ecec58239927bb215eb825
irera <ferreirasdemelo@gmail.com>
2025-07-07 19:10:20 -0300

    feat(main): criação do banco de dados na primeira execução

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
+from data_handler import *
+from db_functions import *
+from user_interface import *
+
 def main():
-    pass
+    try:
+        pass
+    except sqlite3.OperationalError as e:
+        if "no such table" in str(e):
+            create_db()
 
 if __name__ == "__main__":
     main()
