0fc4ee9fe5bcbe9a00a3dd752efb21e780f82f8a
sferrem <ferreirasdemelo@gmail.com>
2025-07-07 11:22:07 -0300

    feat: create_db, add_task

--- src/data_handler.py
+++ src/data_handler.py
@@ -1,4 +1,33 @@
-import sqlite3
+import sqlite3 as db
 
 def open_connection():
-    pass
+    connection = db.connect("todo.db")
+    return connection.cursor()
+
+def close_connection(curso):
+    
+    curso.close()
+
+def create_db():
+    connection = db.connect("todo.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tasks (
+        task_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        task_name VARCHAR(40),
+        task_description VARCHAR(100)
+    )
+    """)
+    cursor.commit()
+    cursor.close()
+    connection.close()
+
+def add_task(task_name, task_description):
+    connection = db.connect("todo.db")
+    cursor = connection.cursor()
+    open_connection().execute("""
+        INSERT INTO tasks (task_name, task_description) VALUES (?,?)
+    """, (task_name, task_description))
+    cursor.commit()
+    cursor.close()
+    connection.close()  
