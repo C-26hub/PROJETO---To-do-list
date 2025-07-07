03905b5d77f2f3207165208a3c81a27f496efdbc
irera <ferreirasdemelo@gmail.com>
2025-07-07 19:01:31 -0300

    refactor(execute, add_task): encapsular abrir e fechar conexão, adicionar tarefa

---
+++ src/db_functions.py
@@ -0,0 +1,44 @@
+import sqlite3 as db
+
+def execute(script, *args):
+    connection = db.connect("to_do.db")
+    cursor = connection.cursor()
+    cursor.execute(script, args)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+## system config
+def create_db():
+    execute("""
+    CREATE TABLE IF NOT EXISTS tasks (
+        task_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        task_name VARCHAR(40),
+        task_description VARCHAR(100),
+        deadline DATE NOT NULL,
+        status INTEGER CHECK (status IN (0,1)) NOT NULL
+    )
+    """)
+    
+## create
+def add_task(task_name, deadline, task_description=None, status="pendente"):
+    execute("""
+        INSERT INTO tasks (task_name, task_description, deadline, status) VALUES (?,?,?,?,?)
+    """, task_name, task_description, deadline, status)
+
+## read
+
+## select all tasks
+## select done tasks
+## select pending tasks
+
+## update
+
+## rename task
+## rewrite description
+## change deadline
+## mark done/pending
+
+## delete
+
+## delete tasks by id
