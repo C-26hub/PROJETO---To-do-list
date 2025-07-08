4cb4841d86aac17309296308b53b4ad6f43c4709
irera <ferreirasdemelo@gmail.com>
2025-07-08 16:29:49 -0300

    feat(data_handler): adição de funções que serão chamadas pelo front-end
    create_task e get_tasks

--- src/data_handler.py
+++ src/data_handler.py
@@ -3,3 +3,17 @@ from db_functions import *
 def toggle_task_status(task_id):
     task_new_status = 0 if bool(get_task_status()) else 1
     set_task_status(task_id, task_new_status)
+
+## create
+def create_task(task_description, deadline):
+    add_task(task_description, deadline)
+
+## read
+def get_tasks():
+    tasks = select_all_tasks()
+    ## processamento ou conversão
+    return tasks
+
+## update
+
+## delete
