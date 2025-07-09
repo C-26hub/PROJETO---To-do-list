3fd60f079c3236472b1822d8302320e01a8d2bf5
Sam Ferreira <ferreirasdemelo@gmail.com>
2025-07-09 17:03:04 -0300

    refactor(data_handler): fazer voltar conversão de int para bool
    usar conversão é mais eficiente e demonstra mais domínio sobre a linguagem de programação

--- src/data_handler.py
+++ src/data_handler.py
@@ -1,11 +1,6 @@
 from db_functions import *
 from datetime import datetime 
 
-def toggle_task_status(task_id):
-   current_status = get_task_status(task_id)
-   new_status = 1 if current_status == 0 else 0
-   set_task_status(task_id, new_status)
-
 ## create
 def create_task(task_description, deadline):
     add_task(task_description, deadline)
@@ -16,13 +11,16 @@ def get_tasks():
     formatted_tasks = []
     for task in tasks:
         task_id, description, deadline, status_code = task
-        status_text = "Pendente" if status_code == 0 else "Concluída"
+        status_text = "Concluída" if bool(status_code) == 0 else "Pendente"
         formatted_line = f"ID:  {task_id} Status: {status_text} Prazo: {deadline} Tarefa: {description}"
         formatted_tasks.append(formatted_line)
     ## processamento ou conversão
     return formatted_tasks
 
 ## update
+def toggle_task_status(task_id):
+    new_status = 0 if bool(get_task_status(task_id)) else 1
+    set_task_status(task_id, new_status)
 def update_task_description(task_id, new_description):
     set_task_description(task_id, new_description)
 
