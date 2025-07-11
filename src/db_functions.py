c821430ca8b013352a392e161fb408bf3a40cc61
irera <ferreirasdemelo@gmail.com>
2025-07-11 01:21:47 -0300

    feat(db_functions): adicionar task_name por meio da função add_task

--- src/db_functions.py
+++ src/db_functions.py
@@ -32,11 +32,10 @@ def create_db():
     
 ## create
 
-def add_task(task_description=None, deadline, status=0):
+def add_task(task_name, task_description=None, deadline, status=0):
     execute("""
-        INSERT INTO tasks (task_description, deadline, status) VALUES (?,?,?)
-    """, task_description, deadline, status)
-
+        INSERT INTO tasks (task_name, task_description, deadline, status) VALUES (?,?,?,?)
+    """, task_name, task_description, deadline, status)
 
 ## read
 
